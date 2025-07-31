ef249cbb8588a511aa800917d67f1d6cbd22f777
Jehu Frayle <jehufrayle@gmail.com>
2025-07-30 18:25:16 -0700

    Fixed issue with my twitter pic

--- src/config/social.js
+++ src/config/social.js
@@ -2,7 +2,7 @@ export default {
   networks: [
     {
       name: 'youtube',
-      url: 'https://www.youtube.com/@jehufrayle?sub_confirmation=1',
+      url: 'https://www.youtube.com/@jehu-dev?sub_confirmation=1',
       svg: '<svg class= "transition-all w-6 h-6 cursor-pointer text-zinc-400 hover:text-[#FF0000] fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>YouTube</title><path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"></path></svg> ',
     },
     {
@@ -17,7 +17,7 @@ export default {
     // },
     {
       name: 'x',
-      url: 'https://x.com/JehuFrayle',
+      url: 'https://x.com/jehu_dev',
       svg: '<svg class="transition-all w-6 h-6 cursor-pointer text-zinc-400 hover:text-blue-300 fill-current" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg"><title>X</title><path d="M 11 4 C 7.134 4 4 7.134 4 11 L 4 39 C 4 42.866 7.134 46 11 46 L 39 46 C 42.866 46 46 42.866 46 39 L 46 11 C 46 7.134 42.866 4 39 4 L 11 4 z M 13.085938 13 L 21.023438 13 L 26.660156 21.009766 L 33.5 13 L 36 13 L 27.789062 22.613281 L 37.914062 37 L 29.978516 37 L 23.4375 27.707031 L 15.5 37 L 13 37 L 22.308594 26.103516 L 13.085938 13 z M 16.914062 15 L 31.021484 35 L 34.085938 35 L 19.978516 15 L 16.914062 15 z"></path></svg>',
     },
     {
